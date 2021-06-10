c0b81ef67010a4e1f175f175fe48ae57db257edb
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-10 02:24:07 -0400

    Homeview and nav header

---
+++ Drachma/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleButtonAnimationView.swift
+//  Drachma
+//
+//  Created by Vivek Pattanaik on 6/10/21.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    @Binding var animate : Bool
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none)
+    }
+}
+
+struct CircleButtonAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimationView(animate: .constant(false))
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
