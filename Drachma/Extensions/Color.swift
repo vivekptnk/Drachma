ba70cd12456f50bf3e6815032fd1ba0050c5469b
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-10 01:52:47 -0400

    Color Theme and Project Setup

---
+++ Drachma/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  Drachma
+//
+//  Created by Vivek Pattanaik on 6/10/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let red = Color("RedColor")
+    let green = Color("GreenColor")
+    let secondaryText = Color("secondaryTextColor")
+}
