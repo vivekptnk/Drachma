cf2e89384764f6ad4ce09d77ad7476c991ab36a7
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-10 01:35:27 -0400

    Initial Commit

---
+++ Drachma/DrachmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrachmaApp.swift
+//  Drachma
+//
+//  Created by Vivek Pattanaik on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DrachmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
