011ed65de494f3bc75c3d308314c706a6a76ae3a
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-15 01:01:24 -0400

    Coin Service Ready

--- Drachma/Core/Home/ViewModels/HomeViewModel.swift
+++ Drachma/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,17 +6,21 @@
 //
 
 import Foundation
-
+import Combine
 class HomeViewModel : ObservableObject {
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
-    
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
     init(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
-            self.allCoins.append(DeveloperPreview.instance.coin)
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?. allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
--- Drachma/Services/CoinDataService.swift
+++ Drachma/Services/CoinDataService.swift
@@ -6,9 +6,13 @@
 //
 
 import Foundation
+import Combine
+
 
 class CoinDataService {
     @Published var allCoins : [Coin] = []
+    var coinSubscription : AnyCancellable?
+    
     init(){
         getCoins()
     }
@@ -16,7 +20,7 @@ class CoinDataService {
     private func getCoins(){
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {return}
         
-        URLSession.shared.dataTaskPublisher(for: url)
+        coinSubscription =  URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { (output) -> Data in
                 guard let response = output.response as? HTTPURLResponse,
@@ -27,6 +31,21 @@ class CoinDataService {
             }
             .receive(on: DispatchQueue.main)
             .decode(type: [Coin].self, decoder: JSONDecoder())
+            .sink { (completion) in
+                switch completion{
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+                
+                
+            } receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            }
+//            .store(in: &cancellables)
+
 
     }
 }
