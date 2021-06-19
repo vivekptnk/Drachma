5dd3eb100dd084aebd96fdb4fb8f014104dce34d
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-18 21:54:04 -0400

    File Manager Caching done

--- Drachma/Services/CoinImageService.swift
+++ Drachma/Services/CoinImageService.swift
@@ -14,13 +14,27 @@ class CoinImageService {
     @Published var image : UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: Coin
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName : String
     init(coin : Coin) {
         self.coin = coin
-        self.getCoinImage()
+        self.imageName = coin.id
+        getCoinImage()  
     }
     
     private func getCoinImage() {
-        print("Dowloading Image now")
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from File Manager")
+        } else {
+            downloadCoinImage()
+            print("Download image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
+        
         guard let url = URL(string: coin.image) else {return}
         
         imageSubscription =
@@ -29,8 +43,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else {return}
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
